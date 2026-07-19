51dc94aac776c055ed3a276d37aaa7487c48c68e
王兴家 <xingwangzhe@outlook.com>
2026-07-19 16:42:19 +0800

    fix: use bun x napi in CI + drop x86 macOS
    Signed-off-by: 王兴家 <xingwangzhe@outlook.com>

--- index.cjs
+++ index.cjs
@@ -4,7 +4,6 @@ const path = require("path");
 const targets = {
   "win32-x64": "mermaid-rs.win32-x64-msvc.node",
   "win32-arm64": "mermaid-rs.win32-arm64-msvc.node",
-  "darwin-x64": "mermaid-rs.darwin-x64.node",
   "darwin-arm64": "mermaid-rs.darwin-arm64.node",
   "linux-x64": "mermaid-rs.linux-x64-gnu.node",
   "linux-arm64": "mermaid-rs.linux-arm64-gnu.node",
